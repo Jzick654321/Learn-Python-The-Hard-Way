99494c8ed249727ee091f98a14fb6f9a76e57bb3
James Zick <student.139954@worcesterschools.net>
2019-05-13 13:03:33 +0000

    Second Commit

--- ex19.py
+++ ex19.py
@@ -4,18 +4,18 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
 
-
+# printing
 print("We can just give the function numbers directly:")
 cheese_and_crackers(20, 30)
 
-
+# printing and giving amount of cheese and crackers.
 print("OR, we can use variables from our script:")
 amount_of_cheese = 10
 amount_of_crackers = 50
 
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
-
+# adding cheese and crackers.
 print("We can even do math inside too:")
 cheese_and_crackers(10 + 20, 5 + 6)
 
--- ex21.py
+++ ex21.py
@@ -1,33 +1,33 @@
 def add(a, b):
     print(f"ADDING {a} + {b}")
     return a + b
-
+# subtracting a from b
 def subtract(a, b):
     print(f"SUBTRACTING {a} - {b}")
     return a - b
-
+# multiplying from a to b
 def multiply(a, b):
     print(f"MULTIPLYING {a} * {b}")
     return a * b
-
+# dividing a from b
 def divide(a, b):
     print(f"DIVIDING {a} / {b}")
     return a / b
 
-
+# printing
 print("Let's do some math with just functions!")
-
+# giving numbers for a and b
 age = add(30, 5)
 height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
-
+# printing
 print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 
 
 # A puzzle for the extra credit, type it in anyway.
 print("Here is a puzzle.")
-
+# adding, subtracting, multiplying, and dividing.
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
-
+# printing.
 print("That becomes: ", what, "Can you do it by hand?")
